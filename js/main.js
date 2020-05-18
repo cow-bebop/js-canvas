90e7a230cff1d0a57ccc2fde5976e1cdc04776b8
けお <o.kengo1987@gmail.com>
2020-05-18 23:34:16 +0900

    最初のコミット

---
+++ js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+{
+  function draw() {
+    const canvas = document.querySelector("canvas");
+    if (typeof canvas.getContext === "undefined") {
+      return;
+    }
+    const ctx = canvas.getContext("2d");
+
+    // ctx.fillRect(x, y, width, height);
+    ctx.fillStyle = "pink";
+    ctx.fillRect(50, 50, 50, 50);
+    ctx.strokeStyle = "#f00";
+    ctx.lineWidth = 8;
+    // ctx.lineJoin = "round";
+    ctx.lineJoin = "bevel";
+    ctx.strokeRect(50, 50, 50, 50);
+  }
+
+  draw();
+}
